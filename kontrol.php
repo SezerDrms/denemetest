7f327c9824a6f00a0641085fc6bfe2d3aee32590
SezerDrms <sezerdrms@gmail.com>
2013-12-11 18:14:11 +0200

    a few changes

--- kontrol.php
+++ kontrol.php
@@ -1,31 +1,31 @@
 <?php
 require_once "vt.php";
 if($_POST){
-    $my_name=$_POST["myname"];
+    $my_mail=$_POST["mymail"];
     $password=sha1($_POST["mypassword"]);
     $my_password=$_POST["mypassword"];
-    if(!$my_name && !$my_password){
+    if(!$my_mail && !$my_password){
         Header('Location: index.php?e=d');
     }
-    elseif(!$my_name){
-        Header('Location: index.php?e=n1');
+    elseif(!$my_mail){
+        Header('Location: index.php?e=m1');
     }
     elseif(!$my_password){
         Header('Location: index.php?e=p1');
     }else{
-        $select_name=mysql_query("SELECT * FROM member WHERE member_mail='".$my_name."'");
-        $sql_dizi_name=mysql_num_rows($select_name);
-        if($sql_dizi_name==0&& strlen($my_name)>6){
+        $select_mail=mysql_query("SELECT * FROM member WHERE member_mail='".$my_mail."'");
+        $sql_dizi_mail=mysql_num_rows($select_mail);
+        if($sql_dizi_mail==0&& strlen($my_mail)>6){
             Header('Location: index.php?f=0');
         }elseif($password!=$mepas&& strlen($password)>8){
                 Header('Location: index.php?f=0');
         }else{
-            $select=mysql_query("SELECT * FROM member WHERE member_mail='".$my_name."'");
+            $select=mysql_query("SELECT * FROM member WHERE member_mail='".$my_mail."'");
             $sql_dizi=mysql_num_rows($select);
+            $sql_row = mysql_fetch_array($select);
             if($sql_dizi>0){
             session_start();
-            $user_name=mysql_query("SELECT member_name FROM member WHERE member_mail='".$my_name."'");
-            $_SESSION["userdata"]["mail"] = $user_name;
+            $_SESSION["userdata"]["name"] =  $sql_row["member_name"];
             Header('Location: index.php');
             }else{
                 Header('Location: index.php?user=f');
